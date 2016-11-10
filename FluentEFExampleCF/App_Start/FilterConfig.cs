d7990752cf8bf69f6a5e8116dbd4691857697d60
ItsPaul <powell.paul@itsligo.ie>
2016-11-10 18:11:58 +0000

    Add project files.

---
+++ FluentEFExampleCF/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FluentEFExampleCF
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
